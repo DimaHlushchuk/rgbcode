f2d5c88f743ca35a179487cc4d04c1e850243065
DimaHlushchuk <dmitriy.h@avega-group.com>
2021-10-06 11:09:02 +0300

    Hide form

--- wp/wp-content/plugins/kids_clothes/templates/page-front.php
+++ wp/wp-content/plugins/kids_clothes/templates/page-front.php
@@ -59,25 +59,25 @@ get_header(); ?>
             <?php $user = wp_get_current_user(); ?>
             <?php $allowed_roles = array('editor', 'administrator'); ?>
             <?php if (array_intersect($allowed_roles, $user->roles)) :  ?>
-                <form id="new_post" name="new_post" method="post">
-
-                    <p><label for="title">Title</label><br />
-                        <input type="text" id="title" value="" tabindex="1" size="20" name="title" />
-                    </p>
-
-                    <p><label for="description">Description</label><br />
-                        <textarea id="description" tabindex="3" name="description" cols="50" rows="6"></textarea>
-                    </p>
-
-                    <p><?php wp_dropdown_categories('show_option_none=Category&tab_index=4&taxonomy=clothes-type&hide_empty=0&multiple=true'); ?></p>
-
-                    <p><input type="submit" value="Publish" tabindex="6" id="submit" name="submit" /></p>
-
-                    <input type="hidden" name="post_type" id="post_type" value="clothes" />
-                    <input type="hidden" name="action" value="post" />
-                    <?php wp_nonce_field('new-post'); ?>
-
-                </form>
+<!--                <form id="new_post" name="new_post" method="post">-->
+<!---->
+<!--                    <p><label for="title">Title</label><br />-->
+<!--                        <input type="text" id="title" value="" tabindex="1" size="20" name="title" />-->
+<!--                    </p>-->
+<!---->
+<!--                    <p><label for="description">Description</label><br />-->
+<!--                        <textarea id="description" tabindex="3" name="description" cols="50" rows="6"></textarea>-->
+<!--                    </p>-->
+<!---->
+<!--                    <p>--><?php //wp_dropdown_categories('show_option_none=Category&tab_index=4&taxonomy=clothes-type&hide_empty=0&multiple=true'); ?><!--</p>-->
+<!---->
+<!--                    <p><input type="submit" value="Publish" tabindex="6" id="submit" name="submit" /></p>-->
+<!---->
+<!--                    <input type="hidden" name="post_type" id="post_type" value="clothes" />-->
+<!--                    <input type="hidden" name="action" value="post" />-->
+<!--                    --><?php //wp_nonce_field('new-post'); ?>
+<!---->
+<!--                </form>-->
             <?php endif; ?>
         </div>
     </div>
